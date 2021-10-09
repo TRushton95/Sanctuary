5e402eadcd582e42af4545a23333e76ddf5610aa
Tom <tom.rushton@hotmail.co.uk>
2021-10-09 13:05:44 +0100

    Create custom timer class to allow starting mid progress

---
+++ Src/Gameplay/Utility/Stopwatch.gd
@@ -0,0 +1,42 @@
+extends Node
+class_name Stopwatch
+
+var duration := -1.0 setget ,get_duration
+var current_time := -1.0
+var is_running := false setget ,get_is_running
+
+signal expired
+signal started
+signal stopped
+
+
+func _process(delta: float) -> void:
+	if !is_running:
+		return
+		
+	current_time += delta
+	
+	if current_time > duration:
+		is_running = false
+		emit_signal("expired")
+
+
+func start(duration: float, current_time := 0.0) -> void:
+	self.duration = duration
+	self.current_time = current_time if current_time > 0.0 else duration
+	is_running = true
+	emit_signal("started")
+
+
+func stop() -> void:
+	is_running = false
+	emit_signal("stopped")
+
+
+# Setgetters
+func get_duration() -> float:
+	return duration
+
+
+func get_is_running() -> bool:
+	return is_running
